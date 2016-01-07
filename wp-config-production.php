7bb73cef123d5a23a4dda5dd19e5ee38af23c9bd
Markus Reiter <me@reitermark.us>
2016-01-07 07:25:14 +0100

    Add build scripts.

---
+++ wp-config-production.php
@@ -0,0 +1,14 @@
+<?php
+
+  define('DB_NAME',          'editiont_irol_wp');
+  define('DB_USER',          'editiont_irol');
+  define('DB_PASSWORD',      'UX8fST6q86KcXXJ6u8K8MMHx');
+
+  define('AUTH_KEY',         '[=OyLdw+[OXV;`IlhF_N]`]R={zBeHsNcfF+mWNrRHyIHI#lX`x6Ic9Rl[?NrQQf');
+  define('SECURE_AUTH_KEY',  '6+{yg-AeiA1|-*R zxS9+i.{LJ+w`ny25uE7Z6H#$zz9F{o0J`LZ*K;Rqq@P_|vf');
+  define('LOGGED_IN_KEY',    '1,IM_Fo3xExh3-!uTXUMv)r:Gb[AsxV{I+>rb@PD_`mA-VRH0hR*&Bjmvl-cD]$r');
+  define('NONCE_KEY',        'w @gH~r^&DZ->W~r8]-~*c+%)S<]~35Dn*?g3XiZ1=?Z.hdRF=Rfoh{BPD@1[rgW');
+  define('AUTH_SALT',        '|Q|#-@,S;_$uT|JnF$l6,o4}Vg6w>Kc:Db$k[xx?$cd(X7U}6;>k`<jP?Lrp/l,{');
+  define('SECURE_AUTH_SALT', '<IN-ZtNs&V?TK%z>Pf .wU41OXxQcT*79_W:v<iV MXn?-|tO5|[3EpE1of/g2ch');
+  define('LOGGED_IN_SALT',   '^FDk,?9K7k:9v6%|+^|&:!fiumD|Blo^R7~K:el~zHLj?T.(hGs2;zdYS(GEfpTh');
+  define('NONCE_SALT',       '*IF>06f=k!@t!7Y%;>={4>Q#.MX[,vs~!}ot H6J*QH?vXcpW<(R[Sp;;X`,uC0Y');
